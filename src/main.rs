370c87653b6708e2fc38114f072df72595e19c6b
adam-jb <adam_bricknell@live.com>
2023-05-03 17:22:05 +0000

    handy print statement added to main

--- src/main.rs
+++ src/main.rs
@@ -101,13 +101,15 @@ async fn main() -> std::io::Result<()> {
     }
 
     // comment this out to not make the lookup of nodes which are near other nodes
-    // this is big preprocessing stage (~90mins with 8cores)
+    // this is big preprocessing stage (~90mins with 8cores for 120 seconds)
     if true {
         for time_seconds in [120, 180, 240, 300] {
+            let now = Instant::now();
             let (graph_walk, graph_pt) = read_files_parallel_excluding_node_values(year);
             make_and_serialise_nodes_within_n_seconds::make_and_serialise_nodes_within_n_seconds(
                 Cost(time_seconds), graph_walk, graph_pt,
             );
+            println!("All nearby nodes for {} seconds took {:?} seconds", time_seconds, now.elapsed());
         }
     }
 
